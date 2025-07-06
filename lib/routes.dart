697ad52a56a107879d8d5b6d13bb1bd8c85d23f2
CarlosBanda <c.banda07@hotmail.com>
2025-07-05 22:57:11 -0600

    system file

---
+++ lib/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:padel_app/views/screens/login_page.dart';
+
+Map<String, Widget Function(BuildContext)> routes = {
+  'login': (BuildContext context) => LoginPage(),
+};
